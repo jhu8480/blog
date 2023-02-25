dc5c861c891778b337328cbfb14538096e6c2a82
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-02-25 16:37:47 -0500

    created basic file structure

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config({path: path.join(__dirname, '..', 'config.env')})
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+  }
+);
+
+module.exports = sequelize;
