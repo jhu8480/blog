b8ff81d9b190bce82ee1c0248aabe68f90e3ea24
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-02-25 20:44:59 -0500

    added login and logout routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const router = require('express').Router();
 const userRoutes = require(path.join(__dirname, 'user-routes.js'));
+const blogRoutes = require(path.join(__dirname, 'blog-routes.js'));
+
 
 router.use('/users', userRoutes);
-
+router.use('/blog', blogRoutes);
 
 module.exports = router;
