1633e48a8ad9c03ad295d09e9a9bdc856285e610
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-02-27 19:27:14 -0500

    added commenting feature and get comments by blog id route

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,6 +1,19 @@
 const router = require('express').Router();
 const {User, Blog, Comment} = require('../../models/index');
 
+router.get('/:id', async (req, res) => {
+  try {
+    const response = await Comment.findAll({
+      where: {blog_id: req.params.id},
+      include: [{model: Blog}, {model: User}]
+    });
+    res.status(200).json(response);
+
+  } catch(e) {
+    res.status(500).json(e);
+  }
+});
+
 router.post('/', async (req, res) => {
   try {
     const newComment = Object.assign({}, req.body);
--- public/js/comments.js
+++ public/js/comments.js
@@ -4,9 +4,11 @@ const cancelButtons = document.querySelectorAll('.give-up-comment');
 const commentTitleInput = document.querySelectorAll('.comment_title');
 const commentBodyInput = document.querySelectorAll('.article_comment');
 const anchors = document.querySelectorAll('a[target]');
+const viewCommentsBtn = document.querySelectorAll('.view-comments');
+const closeViewComments = document.querySelectorAll('.close-view-comments');
 
 writeCommentBtn.forEach((button) => {
-  addEventListener('click', (e) => {
+  button.addEventListener('click', (e) => {
     e.preventDefault();
     e.stopPropagation();
     e.target
@@ -77,3 +79,35 @@ anchors.forEach(a => {
   })
 });
 
+viewCommentsBtn.forEach((button) => {
+  button.addEventListener('click',async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const commentsWrapper = e.target.parentElement.querySelector('.view-comments-wrapper');
+    commentsWrapper.classList.remove('hidden');
+    const articleid = e.target.parentElement.querySelector('h6 .articleid').innerText;
+    const blogId = parseInt(articleid);
+    const data = await fetch(`/api/comments/${blogId}`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
+    });
+    const response = await data.json();
+    const commentsListWrapper = e.target.parentElement.querySelector('.comments-list-wrapper');
+    response.forEach((comment) => {
+      const commentDiv = document.createElement('div');
+      commentDiv.classList.add('comment-div');
+      commentDiv.innerHTML = `<h5>${comment.comment_title}</h5><h6>Created by ${comment.user.username}</h6><p>${comment.comment_body}</p>`;
+      commentsListWrapper.appendChild(commentDiv);
+    });
+  });
+    
+});
+
+closeViewComments.forEach(button => {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    e.target.parentElement.classList.add('hidden');
+    e.target.parentElement.querySelector('.comments-list-wrapper').innerHTML = '';
+  })
+});
