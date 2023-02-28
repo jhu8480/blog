c6bc009f1abe76c4530442003f5df6fd7487dc13
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-02-27 22:20:24 -0500

    finished update article feature

--- public/js/manage-content-page.js
+++ public/js/manage-content-page.js
@@ -3,6 +3,43 @@ const deleteBlogBtn = document.querySelectorAll('.delete-blog');
 const deleteCommentBtn = document.querySelectorAll('.delete-comment');
 const updateBlogBtn = document.querySelectorAll('.update-blog');
 const updateCommentBtn = document.querySelectorAll('.update-comment');
+const leaveBtn = document.getElementById('leave-btn');
+const postBtn = document.getElementById('post-btn');
+const userId = parseInt(document.getElementById('user-id-container').innerText);
+
+let blogIdToUpdate;
+
+postBtn.addEventListener('click', async (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  const newTitle = document.getElementById('blog_title').value;
+  const newBody = document.getElementById('blog_body').value;
+  const data = await fetch(`/api/blog/${blogIdToUpdate}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      id: blogIdToUpdate,
+      blog_title: newTitle,
+      blog_body: newBody,
+      user_id: userId
+    }),
+    headers: { 'Content-Type': 'application/json' }
+  });
+  const response = await data.json();
+  if (response.status === 'success') {
+    document.getElementById('blog_title').value = '';
+    document.getElementById('blog_body').value = '';
+    document.querySelector('.update-block').classList.add('hidden');
+    document.location.reload();
+  } else {
+    alert('Fail to update, please try again');
+  }
+});
+
+leaveBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  document.querySelector('.update-block').classList.add('hidden');
+});
 
 goBackButton.addEventListener('click', (e) => {
   e.preventDefault();
@@ -11,11 +48,15 @@ goBackButton.addEventListener('click', (e) => {
 });
 
 updateBlogBtn.forEach(button => {
-  button.addEventListener('click', (e) => {
+  button.addEventListener('click', async (e) => {
     e.preventDefault();
     e.stopPropagation();
+    document.querySelector('.update-block').classList.remove('hidden');
+    blogIdToUpdate = e.target.parentElement.querySelector('.blog-id').innerText;
+    document.getElementById('blog-id-container').innerText = blogIdToUpdate;
   });
 });
+
 deleteBlogBtn.forEach(button => {
   button.addEventListener('click', async (e) => {
     e.preventDefault();
